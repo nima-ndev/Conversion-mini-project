6dea1796388105c61758a238e63fe3d70a3d0245
Nima Nakhaei <133752979+nima-ndev@users.noreply.github.com>
2023-07-07 20:23:55 +0330

    nima commit

--- js/app.js
+++ js/app.js
@@ -3,8 +3,27 @@ let resetButton = document.querySelector(".resetButton");
 let changeButton = document.querySelector(".changeButton");
 let result = document.querySelector(".result");
 let converterInput = document.querySelector("#converter");
+let label= document.querySelector('label');
 let isTempC = true;
 convertButton.addEventListener("click", converting);
+changeButton.addEventListener('click',function(){
+  if(isTempC){
+    label.innerText='Converter °F to °C';
+    converterInput.setAttribute('placeholder','°F');
+    isTempC= false;
+  }
+  else{
+    label.innerText='Converter °C to °F';
+    converterInput.setAttribute('placeholder','°C');
+    isTempC= true;
+  }
+});
+resetButton.addEventListener('click',function(){
+  converterInput.value = "";
+  result.innerHTML="";
+});
+
+
 
 function converting() {
   if (isTempC) {
@@ -19,7 +38,7 @@ function convertCtoF() {
   converterInput.value = "";
 }
 function convertFtoC() {
-  let convert = (+input.value - 32) / 1.8;
-  result.innerHTML = +converterInput.value + "°C" + " is: " + convert + "°F";
+  let convert = (+converterInput.value - 32) / 1.8;
+  result.innerHTML = +converterInput.value + "°F" + " is: " + convert + "°C";
   converterInput.value = "";
 }
